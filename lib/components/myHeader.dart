5f6ec0ac6fe19ee73b616f7d28af8f4b3e4cc8f9
kytsi <vladyslavkutsym@studenci.collegiumwitelona.pl>
2025-03-22 13:22:23 +0100

    login&signup

---
+++ lib/components/myHeader.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class  Myheader  extends StatelessWidget {
+  const  Myheader ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+      padding: EdgeInsets.symmetric(horizontal: 5 , vertical: 5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+
+          //Button for return to home
+          IconButton(
+            onPressed: () {
+
+            },
+            icon: Icon(Icons.arrow_back_ios_new, color: Colors.black),
+          ),
+
+          Row(
+            children: [
+
+              //button login
+              ElevatedButton(
+                onPressed: (){
+                  Navigator.pushNamed(context, '/login');
+                },
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.black,
+                  backgroundColor: Colors.grey[300],
+                  padding: EdgeInsets.symmetric(horizontal: 20 , vertical: 10),
+                ),
+                child: Text("Log in"),
+              ),
+
+              SizedBox(width: 10),
+
+              //button signup
+              ElevatedButton(
+                onPressed: (){
+                  Navigator.pushNamed(context, '/signUp');
+                },
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.blue,
+                  padding: EdgeInsets.symmetric(horizontal: 20 , vertical: 10),
+                ),
+                child: Text("Sign up"),
+              ),
+
+              SizedBox(width: 10),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/pages/signPage.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:vama_mobile/components/myButton.dart';
+import 'package:vama_mobile/components/myTextfield.dart';
+import 'package:vama_mobile/components/myHeader.dart';
+
+class SignPage extends StatefulWidget{
+  SignPage ({super.key});
+
+  @override
+  State<SignPage> createState() => _SignPageState();
+}
+class _SignPageState extends State<SignPage> {
+  //text editing controllers
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  //sign user in method
+  void signUserIn(){}
+
+  //checkbox
+  bool _isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+            child:Center(
+                child:Column(
+                  children: [
+
+                    //Header
+                    Myheader(),
+
+                    const SizedBox(height: 30,),
+
+                    //Welcome
+                    Text(
+                      "Welcome to VAMA!",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 32,
+                      ),
+                    ),
+
+                    //Second Text
+                    Text(
+                      "Please sign up.",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 24,
+                      ),
+                    ),
+
+                    const SizedBox(height: 20,),
+
+                    //Username Textfield
+                    MyTextField(
+                      controller: usernameController,
+                      hintText: 'Username',
+                      obscureText: false,
+                    ),
+
+                    const SizedBox(height: 20,),
+
+                    //Password Textfield
+                    MyTextField(
+                      controller: passwordController,
+                      hintText: 'Password',
+                      obscureText: true,
+                    ),
+
+                    const SizedBox(height: 20,),
+
+                    //Checkbox
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Checkbox(
+                          value: _isChecked,
+                          onChanged: (bool? value) {
+                            setState(() {
+                              _isChecked = value ?? false;
+                            });
+                          },
+                        ),
+                        Flexible(
+                          child: Text(
+                            "By checking you accept the Terms of condition",
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    const SizedBox(height: 10,),
+
+                    //sign in
+                    Mybutton(
+                      onTap: signUserIn,
+                    ),
+                  ],
+                )
+            )
+        )
+    );
+  }
+}
